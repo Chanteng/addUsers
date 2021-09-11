0998a1ad0322f1e2f3f7a88ceadb4b247f4db47f
Chanteng <yrnchanteng@gmail.com>
2021-09-11 11:35:19 -0700

    fix usestate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import UserForm from './Components/UserForm';
 import UserList from './Components/UserList';
-import { useState } from 'react/cjs/react.development';
 
 function App() {
 
